01c825a6c9487854e0b0e03d6f670efd0487d548
sophieyang0813@gmail.com <sophieyang@Sophies-MacBook-Pro.local>
2017-10-19 11:43:08 +0800

    1) created show.html.erb 2) defined in show in controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,4 +4,20 @@ class UrlsController < ApplicationController
         @urls = Url.all
     end
 
+
+    def show
+        @url = Url.find(params[:id])
+    end
+
+    def create
+        @url = Url.new(params[:url])
+        @url.shorten
+        if @url.save
+            redirect_to @url
+        else
+            render 'new'
+        end
+    end
+
+
 end
